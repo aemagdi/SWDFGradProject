38cde701edec38db5c517486cff656fa3f17d6d0
Abdelrahman Magdy <abdmgd1@gmail.com>
2022-03-31 13:18:13 +0200

    changed add admin user table's view

--- resources/views/admin/addmember.blade.php
+++ resources/views/admin/addmember.blade.php
@@ -10,8 +10,31 @@
 </head>
 <body>
 
+    <div class="container-scroller" style="position: relative; top: 0px; right:-10px">
+        @include('admin.navbar')
+        <div class="container-scroller" style="position: relative; top: 110px; right:-250px">
+            <form action="/store" method="post" enctype="multipart/form-data">
+                @csrf
+                <div>
+                    <label>Name</label>
+                    <input type="text" style="color:blue" name="name" placeholder="Enter user's name" required>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" style="color:blue" name="email" placeholder="Enter user's email" required>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" style="color:blue" name="password" placeholder="Enter user's password"  required>
+                </div>
 
-  <div class="container-scroller">
+
+                <div style= "color:white">
+                    <input type="submit" value="submit">
+                </div>
+            </form>
+        </div>
+  {{-- <div class="container-scroller">
 
   @include('admin.navbar')
 
@@ -41,7 +64,7 @@
 </div>
 </form>
 
-  </div>
+  </div> --}}
 
 
   @include('admin.adminscript')
